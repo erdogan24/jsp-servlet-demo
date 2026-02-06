09dadf08826482c78a5d3830b9320ec4bd669bee
erdoganakpolat <erdoganakpolat98@gmail.com>
2026-02-06 03:23:22 +0300

    new commit

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -13,6 +13,6 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
 
         resp.setContentType("text/html;charset=UTF-8");
-        resp.getWriter().println("<h1>Hello Servlet ✅</h1>");
+        resp.getWriter().println("<h1>Hello Servlet ✅ :)</h1>");
     }
 }
